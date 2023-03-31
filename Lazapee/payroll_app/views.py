6a32d7db67eb40912b29971b24d4f3a84e0349b9
Bastinoroni96 <gsbasti96@gmail.com>
2023-03-31 15:19:03 +0800

    Made new project from scratch

--- Lazapee/payroll_app/views.py
+++ Lazapee/payroll_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'payroll_app/hello_world.html')
+        return render(request, 'payroll_app/hello_world.html')
