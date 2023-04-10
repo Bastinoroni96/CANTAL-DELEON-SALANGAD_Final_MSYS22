8c95db514dba1f8be5721721b55290b19d6b37cb
Bastinoroni96 <gsbasti96@gmail.com>
2023-04-10 14:04:50 +0800

    Created Base template
    Base template will be present in the following created templates with the following features:
    Header with Name
    Navigation bar for Employee and Payslip Pages
    Footer containing year when project was created

--- Lazapee/payroll_app/admin.py
+++ Lazapee/payroll_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Employee, Payslip
 # Register your models here.
+# Register created models here in order to update objects with admin power
 
 admin.site.register(Employee)
 admin.site.register(Payslip)
